bddc732940aadeb32299e0d19c88b0e31a262978
yedewen <yedewen@boco.com.cn>
2017-12-08 02:37:14 +0800

    io流

---
+++ java-demo/src/main/java/com/io/fileoperate/FileLookRepeateLine.java
@@ -0,0 +1,97 @@
+package com.io.fileoperate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by lenovo on 2017/12/8.
+ */
+public class FileLookRepeateLine {
+    private static Logger logger = LoggerFactory.getLogger(FileLookRepeateLine.class);
+    private Map<String,List<Integer>> repeateLineMap = new HashMap<>();
+
+    public void  readFileByBufferedReader(String fileName) {
+
+        BufferedReader bufferedReader = null;
+        FileReader fileReader = null;
+        if (fileName == null) {
+            logger.error("readFileByCharacterStream, fileName is null!");
+            return;
+        }
+        File fileRead = new File(fileName);
+        if (!fileRead.exists()) {
+            try {
+                fileRead.createNewFile();
+            } catch (IOException e) {
+                logger.error("readFileByCharacterStream, fileName is null!");
+                return;
+            }
+        }
+        try {
+
+            fileReader = new FileReader(fileRead);
+            bufferedReader = new BufferedReader(fileReader);
+            String  value= "";
+            int i = 1;
+            while ((value = bufferedReader.readLine()) != null) {
+                if(!repeateLineMap.containsKey(value)) {
+                    List<Integer> lineList = new ArrayList<>();
+                    lineList.add(i);
+                    repeateLineMap.put(value,lineList);
+                }else {
+                    List<Integer> lineList = repeateLineMap.get(value);
+                    lineList.add(i);
+                    repeateLineMap.put(value,lineList);
+                }
+                i++;
+            }
+        } catch (FileNotFoundException e) {
+            logger.error("readFileByCharacterStream,  is error! FileNotFoundException:", e);
+        } catch (IOException e) {
+            logger.error("readFileByCharacterStream,  is error! FileNotFoundException:", e);
+
+        }finally {
+            if(fileReader != null) {
+                try {
+                    fileReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void printRepeateLine(){
+        for(Map.Entry entry  :repeateLineMap.entrySet()) {
+           List<Integer> lineList = (List <Integer>) entry.getValue();
+           if(lineList.size() >1) {
+               if(entry.getKey() ==  null || entry.getKey().toString().trim().length() == 0) {
+                   System.out.println("detail:" +entry.getKey() +",blank lineList:" +entry.getValue());
+               }else {
+                   System.out.println("detail:" +entry.getKey() +",lineList:" +entry.getValue());
+               }
+           }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        FileLookRepeateLine fileLookRepeateLine = new FileLookRepeateLine();
+        String fileName = "fileWriteTest.txt";
+        fileLookRepeateLine.readFileByBufferedReader(fileName);
+        fileLookRepeateLine.printRepeateLine();
+    }
+}
--- java-demo/src/main/java/com/io/fileoperate/FileReadDemo.java
+++ java-demo/src/main/java/com/io/fileoperate/FileReadDemo.java
@@ -3,10 +3,7 @@ package com.io.fileoperate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created by lenovo on 2017/12/7.
@@ -14,29 +11,175 @@ import java.io.IOException;
 public class FileReadDemo {
     private Logger logger = LoggerFactory.getLogger(FileReadDemo.class);
 
-    public void readFile(String filePath) {
+    /**
+     *
+     * @param filePath
+     *
+     * <p>
+     *     按字节流单个字符读取文件，
+     * </p>
+     */
+    public void readFileByBytesStream(String filePath) {
         FileInputStream fileInputStream = null;
         File readFile = new File(filePath);
-        if(!readFile.exists()) {
+        if (!readFile.exists()) {
             logger.error("readFile, file path is not exists!" + filePath);
             return;
         }
         try {
             fileInputStream = new FileInputStream(readFile);
+            int value = 0;
+            byte[] bytes = new byte[(int) readFile.length()];
+            while ((value = fileInputStream.read()) != -1) {
+                System.out.print((char) value);
+
+            }
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }finally {
-            if(fileInputStream!= null) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
                 try {
                     fileInputStream.close();
                 } catch (IOException e) {
-                    logger.error("readFile, fileInputStream.close() is error! IOException:" , e);
+                    logger.error("readFile, fileInputStream.close() is error! IOException:", e);
                 }
             }
 
         }
+    }
 
+    /**
+     *
+     * @param fileName
+     * <p>
+     *     按字符流单个字符读取文件
+     * </p>
+     */
+    public void readFileByCharacterStream(String fileName) {
+        FileReader fileReader = null;
+        if (fileName == null) {
+            logger.error("readFileByCharacterStream, fileName is null!");
+            return;
+        }
+        File fileRead = new File(fileName);
+        if (!fileRead.exists()) {
+            try {
+                fileRead.createNewFile();
+            } catch (IOException e) {
+                logger.error("readFileByCharacterStream, fileName is null!");
+                return;
+            }
+        }
+        try {
+            fileReader = new FileReader(fileRead);
+            int value = 0;
+            while ((value = fileReader.read()) != -1) {
+                System.out.print((char) value);
+            }
+        } catch (FileNotFoundException e) {
+            logger.error("readFileByCharacterStream,  is error! FileNotFoundException:", e);
+        } catch (IOException e) {
+            logger.error("readFileByCharacterStream,  is error! FileNotFoundException:", e);
 
+        }
+    }
+
+    /**
+     *
+     * @param fileName
+     * <p>
+     *     按字符流行读取文件
+     * </p>
+     */
+    public  void readFileByBufferedReader(String fileName) {
+        BufferedReader bufferedReader = null;
+        FileReader fileReader = null;
+        if (fileName == null) {
+            logger.error("readFileByCharacterStream, fileName is null!");
+            return;
+        }
+        File fileRead = new File(fileName);
+        if (!fileRead.exists()) {
+            try {
+                fileRead.createNewFile();
+            } catch (IOException e) {
+                logger.error("readFileByCharacterStream, fileName is null!");
+                return;
+            }
+        }
+        try {
+
+            fileReader = new FileReader(fileRead);
+            bufferedReader = new BufferedReader(fileReader);
+            String  value= "";
+            while ((value = bufferedReader.readLine()) != null) {
+                System.out.print(value);
+            }
+        } catch (FileNotFoundException e) {
+            logger.error("readFileByCharacterStream,  is error! FileNotFoundException:", e);
+        } catch (IOException e) {
+            logger.error("readFileByCharacterStream,  is error! FileNotFoundException:", e);
+
+        }finally {
+            if(fileReader != null) {
+                try {
+                    fileReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void readFileByBufferedInputStream(String filePath) {
+        BufferedInputStream bufferedInputStream = null;
+        FileInputStream fileInputStream = null;
+        BufferedOutputStream bufferedOutputStream = null;
+        File readFile = new File(filePath);
+        if (!readFile.exists()) {
+            logger.error("readFile, file path is not exists!" + filePath);
+            return;
+        }
+        try {
+            fileInputStream = new FileInputStream(readFile);
+            bufferedInputStream = new BufferedInputStream(fileInputStream);
+            int value = 0;
+
+            while ((value = bufferedInputStream.read()) != -1) {
+                System.out.print((char) value);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    logger.error("readFile, fileInputStream.close() is error! IOException:", e);
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        FileReadDemo fileReadDemo = new FileReadDemo();
+        String fileName = "fileWriteTest.txt";
+        fileReadDemo.readFileByBytesStream(fileName);
+        fileReadDemo.readFileByCharacterStream(fileName);
     }
 }
