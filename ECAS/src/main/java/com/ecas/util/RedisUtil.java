2ddb8a0aaa62896d3401a4c848904e40f4b0b356
Devonmusa <Devonmusa2015@gmail.com>
2018-01-28 20:56:45 +0800

    redis 配置文件路径修改

--- ECAS/src/main/java/com/ecas/util/RedisUtil.java
+++ ECAS/src/main/java/com/ecas/util/RedisUtil.java
@@ -22,26 +22,28 @@ public class RedisUtil {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(RedisUtil.class);
 
+	private static  final String REDIS_CONFIG_PATH = "redis/redis";
+
 	// Redis服务器IP
-	private static String IP = PropertiesFileUtil.getInstance("redis").get("master.redis.ip");
+	private static String IP = PropertiesFileUtil.getInstance(REDIS_CONFIG_PATH).get("master.redis.ip");
 
 	// Redis的端口号
-	private static int PORT = PropertiesFileUtil.getInstance("redis").getInt("master.redis.port");
+	private static int PORT = PropertiesFileUtil.getInstance(REDIS_CONFIG_PATH).getInt("master.redis.port");
 
 	// 访问密码
-	private static String PASSWORD = AESUtil.aesDecode(PropertiesFileUtil.getInstance("redis").get("master.redis.password"));
+	private static String PASSWORD = AESUtil.aesDecode(PropertiesFileUtil.getInstance(REDIS_CONFIG_PATH).get("master.redis.password"));
 	// 可用连接实例的最大数目，默认值为8；
 	// 如果赋值为-1，则表示不限制；如果pool已经分配了maxActive个jedis实例，则此时pool的状态为exhausted(耗尽)。
-	private static int MAX_ACTIVE = PropertiesFileUtil.getInstance("redis").getInt("master.redis.max_active");
+	private static int MAX_ACTIVE = PropertiesFileUtil.getInstance(REDIS_CONFIG_PATH).getInt("master.redis.max_active");
 
 	// 控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值也是8。
-	private static int MAX_IDLE = PropertiesFileUtil.getInstance("redis").getInt("master.redis.max_idle");
+	private static int MAX_IDLE = PropertiesFileUtil.getInstance(REDIS_CONFIG_PATH).getInt("master.redis.max_idle");
 
 	// 等待可用连接的最大时间，单位毫秒，默认值为-1，表示永不超时。如果超过等待时间，则直接抛出JedisConnectionException；
-	private static int MAX_WAIT = PropertiesFileUtil.getInstance("redis").getInt("master.redis.max_wait");
+	private static int MAX_WAIT = PropertiesFileUtil.getInstance(REDIS_CONFIG_PATH).getInt("master.redis.max_wait");
 
 	// 超时时间
-	private static int TIMEOUT = PropertiesFileUtil.getInstance("redis").getInt("master.redis.timeout");
+	private static int TIMEOUT = PropertiesFileUtil.getInstance(REDIS_CONFIG_PATH).getInt("master.redis.timeout");
 
 	// 在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
 	private static boolean TEST_ON_BORROW = false;
