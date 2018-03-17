81fe22ad24e516dc4b4b23c9bb984e52a9904505
Devonmusa <Devonmusa2015@gmail.com>
2018-03-17 21:27:01 +0800

    [add] 注解demo

---
+++ java-demo/src/main/java/com/annotation/BussionLogic.java
@@ -0,0 +1,17 @@
+package com.annotation;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class BussionLogic {
+
+    @MyAnnotation(id=5,name = "张三")
+    public void method1(){
+        log.info("method1 is executing...");
+    }
+
+    @MyAnnotation
+    public void method2(){
+        log.info("method2 is executing...");
+    }
+}
