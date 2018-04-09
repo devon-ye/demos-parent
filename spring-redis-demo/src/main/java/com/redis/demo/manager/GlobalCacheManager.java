d62db8e7272171db41b38eb3aedb886c40589ecd
Devonmusa <Devonmusa2015@gmail.com>
2018-04-09 22:15:56 +0800

    [add] spring redis demo

---
+++ spring-redis-demo/src/main/java/com/redis/demo/manager/GlobalCacheManager.java
@@ -0,0 +1,24 @@
+package com.redis.demo.manager;
+
+import org.springframework.cache.Cache;
+import org.springframework.cache.transaction.AbstractTransactionSupportingCacheManager;
+
+import java.util.Collection;
+
+public class GlobalCacheManager extends AbstractTransactionSupportingCacheManager {
+
+    private  Collection<? extends Cache> caches;
+
+    public GlobalCacheManager() {
+    }
+
+    @Override
+    protected Collection<? extends Cache> loadCaches() {
+        return caches;
+    }
+
+
+    public void setCaches(Collection<? extends Cache> caches){
+        this.caches = caches;
+    }
+}
