e4ad0d5f3562fb62185396c16f740b2246220796
Devonmusa <Devonmusa2015@gmail.com>
2018-06-05 20:38:07 +0800

    [UPDATE] update

--- ECAS/README.md
+++ ECAS/README.md
@@ -1,12 +1,3 @@
-# ECAS
 
-## 　简介
-　　educational comprehensive administration system simple name is ECAS
-
-## 项目介绍
-
-## 如何如何参与其中
-## 书籍博客推荐
-## 问题反馈
-
+# 本项目已迁移至 https://github.com/Devonmusa/ecas
 
--- algorithm-demo/src/main/java/com/algorithm/demo/leetcode/SumCompute.java
+++ algorithm-demo/src/main/java/com/algorithm/demo/leetcode/SumCompute.java
@@ -10,71 +10,22 @@ import java.util.*;
  */
 public class SumCompute {
 
-    /**
-     * 思想：比较数组两数组合的可能性大小和目标值拆分为两数可能性大小
-     * 1.如果数组两数组合的数量小于目标值拆分组的可能性,即对数组两数求和跟目标值比较
-     * 2.如果目标值拆分的组数小于数组组成可能性，即在数组中寻找目标值拆分后的值是否存在
-     *
-     * @param nums
-     * @param target
-     * @return
-     */
+
+
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        int targetCombinationSize = target / 2;
-        Integer[] boxedArray = new Integer[nums.length];
+        Map<Integer,Integer> integerMap = new HashMap<>(nums.length);
         for(int i=0,length=nums.length;i<length;i++){
-            boxedArray[i]=nums[i];
-        }
-        Set<Integer> integerSet = new HashSet();
-        integerSet.addAll(Arrays.asList(boxedArray));
-        int arrayCombinationSize = recursive(integerSet.size(),2);
-
-        if (targetCombinationSize <= arrayCombinationSize) {
-
-            List<Integer> numList = new ArrayList<Integer>(Arrays.asList(boxedArray));
-            for (int i = 0; i < targetCombinationSize; i++) {
-
-                if (numList.contains(i) && numList.contains(target - i)) {
-                    result[0] = numList.indexOf(i);
-                    result[1] =numList.indexOf(target - i);
-                }
+            int complement = target - nums[i];
+            if(integerMap.containsKey(complement)){
+                return new int[]{integerMap.get(complement),i};
             }
+            integerMap.put(nums[i],i);
 
-        } else {
-            for (int j = 0,length1=nums.length; j < length1; j++) {
-                for (int k = j + 1; k < length1; k++) {
-
-                    if ((nums[j] + nums[k]) == target) {
-                        result[0] = j;
-                        result[1] = k;
-                    }
-                }
-            }
         }
-        return result;
-
+        ThreadLocal threadLocal = new ThreadLocal();
+        threadLocal.set("");
+        throw new IllegalArgumentException();
     }
-
-    private int recursive(int m, int n) {
-        if (n == 0 || m == 0) {
-            return 0;
-        }
-        if (m < n) {
-            return 0;
-        }
-
-        if(n==1){
-            return m;
-        }
-
-        if (m == n) {
-            return 1;
-        }
-
-        return recursive(m-1 , n) + recursive(m - 1, n - 1);
-    }
-
 
 
     public static void main(String[] args) {
