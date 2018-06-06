6bcd287561591fc4fb42ac7ed8a768cf8728d5de
Devonmusa <Devonmusa2015@gmail.com>
2018-06-06 23:52:13 +0800

    [UPDATE] binaerySearch

--- algorithm-demo/src/main/java/com/algorithm/demo/search/BinaerySearch.java
+++ algorithm-demo/src/main/java/com/algorithm/demo/search/BinaerySearch.java
@@ -8,4 +8,38 @@ package com.algorithm.demo.search;
  */
 public class BinaerySearch {
 
+
+    /**
+     * 1.验空
+     * 2.定义左右边界数下标（）
+     * 3.求中位数下标
+     * 4.比较大小移动左右边界下标
+     * 5.循环步骤3，4
+     * 6.返回结果下标
+     *
+     * @param array
+     * @param key
+     * @return
+     */
+    public static int rank(int[] array, int key) {
+        if (array == null) {
+            return -1;
+        }
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (key < array[mid]) {
+                right = mid-1;
+            } else if (key > array[mid]) {
+                left = mid+1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
 }
