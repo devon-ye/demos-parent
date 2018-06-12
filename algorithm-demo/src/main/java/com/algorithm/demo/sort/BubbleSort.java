4e1492607c13af0d839c9144336de869e3a2a307
Devonmusa <Devonmusa2015@gmail.com>
2018-06-13 00:39:50 +0800

    [UPDATE]

--- algorithm-demo/src/main/java/com/algorithm/demo/sort/BubbleSort.java
+++ algorithm-demo/src/main/java/com/algorithm/demo/sort/BubbleSort.java
@@ -7,4 +7,22 @@ package com.algorithm.demo.sort;
  * @since 1.0.0
  */
 public class BubbleSort implements SortedAlgorithms {
+
+    public static int[] bubbleSort(int[] array) {
+        if (array == null) {
+            return null;
+        }
+        for (int i = 0, length = array.length - 1; i < length; i++) {
+            for (int j = 0; j < length - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+
+        }
+
+        return array;
+    }
 }
