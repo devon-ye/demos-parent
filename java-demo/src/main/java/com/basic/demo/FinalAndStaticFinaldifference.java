8a201d466b42b66fe4d444a4618b46e7fb907fd0
Devonmusa <Devonmusa2015@gmail.com>
2018-06-24 05:33:25 +0800

    [UPDATE]  static

--- java-demo/src/main/java/com/basic/demo/FinalAndStaticFinaldifference.java
+++ java-demo/src/main/java/com/basic/demo/FinalAndStaticFinaldifference.java
@@ -15,4 +15,19 @@ public class FinalAndStaticFinaldifference {
 		
 	}
 
+
+	private static int x = 100;// 2
+
+
+	public static void main(String args[]) {// 3
+		FinalAndStaticFinaldifference hsl = new FinalAndStaticFinaldifference();// 4
+		hsl.x++;// 5
+		FinalAndStaticFinaldifference hs2 = new FinalAndStaticFinaldifference();// 6
+		hs2.x++;// 7
+		hsl = new FinalAndStaticFinaldifference();// 8
+		hsl.x++;// 9
+		FinalAndStaticFinaldifference.x--;// 10
+		System.out.println(" x=" + x);// 11
+	}
+
 }
