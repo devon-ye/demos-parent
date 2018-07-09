e7893ea01aafdd32bc3ecf0fcabadf5e954af9b1
Devonmusa <Devonmusa2015@gmail.com>
2018-07-09 20:32:05 +0800

    [UPDATE]  BASE

--- algorithm-demo/src/main/java/com/algorithm/demo/leetcode/SumCompute.java
+++ algorithm-demo/src/main/java/com/algorithm/demo/leetcode/SumCompute.java
@@ -10,7 +10,7 @@ import java.util.*;
  */
 public class SumCompute {
 
-
+   // 借助辅助空间空间换时间
 
     public int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> integerMap = new HashMap<>(nums.length);
@@ -25,11 +25,23 @@ public class SumCompute {
         throw new IllegalArgumentException();
     }
 
+   //利用时间复杂度换空间
+    public int[] twoSum2(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
 
     public static void main(String[] args) {
         SumCompute sumCompute = new SumCompute();
         int[] array = {0,4,3,0};
-        //TODO FIX BUG
-        sumCompute.twoSum(array,0);
+
+        sumCompute.twoSum(array,4);
     }
 }
