383663d972ec8bbbf9336cbb609e67738843b357
Devonmusa <Devonmusa2015@gmail.com>
2018-07-17 22:21:58 +0800

    [UPDATE]  BASE

---
+++ algorithm-demo/src/main/java/com/algorithm/demo/leetcode/Reverse.java
@@ -0,0 +1,16 @@
+package com.algorithm.demo.leetcode;
+
+/**
+ * @author devonmusa
+ * @description:
+ * @datetime 2018/7/9 20:36
+ * @since 1.0.0
+ */
+public class Reverse {
+
+
+    public int  reverse(int input){
+
+         return Integer.reverse(input);
+    }
+}
