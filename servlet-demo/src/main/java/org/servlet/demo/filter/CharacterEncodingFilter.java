71eafd564d7e6ece4c2b98a6e8e7d0d29f1d720b
devonmusa <Devonmusa2015@gmail.com>
2018-12-14 00:34:34 +0800

    [MODIFY] groupId

--- servlet-demo/src/main/java/org/servlet/demo/filter/CharacterEncodingFilter.java
+++ servlet-demo/src/main/java/org/servlet/demo/filter/CharacterEncodingFilter.java
@@ -11,7 +11,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class CurrentLimitingFilter  implements Filter {
+/**
+ * spring impl
+ */
+public class CharacterEncodingFilter implements Filter {
+
+    private String encoding;
+    private boolean forceEncoding;
+
+
+    public void setForceEncoding(boolean forceEncoding) {
+        this.forceEncoding = forceEncoding;
+    }
+
+    public void setEncoding(String encoding) {
+        this.encoding = encoding;
+    }
+
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -22,6 +38,13 @@ public class CurrentLimitingFilter  implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        if (encoding != null && (servletRequest.getCharacterEncoding() == null || this.forceEncoding)) {
+            httpServletRequest.setCharacterEncoding(this.encoding);
+            if (this.forceEncoding) {
+                httpServletResponse.setCharacterEncoding(this.encoding);
+            }
+
+        }
     }
 
     @Override
--- servlet-demo/src/main/java/org/servlet/demo/filter/LoginAuthBySessionFilter.java
+++ servlet-demo/src/main/java/org/servlet/demo/filter/LoginAuthBySessionFilter.java
@@ -1,6 +1,5 @@
 package org.servlet.demo.filter;
 
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -9,10 +8,11 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-public class CurrentLimitingFilter  implements Filter {
 
+public class LoginAuthBySessionFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -20,8 +20,11 @@ public class CurrentLimitingFilter  implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        HttpSession httpSession = httpServletRequest.getSession();
+
     }
 
     @Override
