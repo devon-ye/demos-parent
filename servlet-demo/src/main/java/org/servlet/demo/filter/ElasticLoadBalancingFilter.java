17cbb6da9560d7f237682e637ba2581f5d7c8c4e
devonmusa <Devonmusa2015@gmail.com>
2018-12-14 00:47:35 +0800

    [ADD] 弹性负载均衡过滤器设想

---
+++ servlet-demo/src/main/java/org/servlet/demo/filter/ElasticLoadBalancingFilter.java
@@ -0,0 +1,29 @@
+package org.servlet.demo.filter;
+
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public class ElasticLoadBalancingFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
