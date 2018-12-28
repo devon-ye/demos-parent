c73952d3e97fa594ea51dd3e8585e64c7f37c3a4
devonmusa <Devonmusa2015@gmail.com>
2018-12-28 23:03:41 +0800

    [ADD] 弹性负载均衡过滤器设想

--- servlet-demo/src/main/java/org/servlet/demo/filter/CurrentLimitingFilter.java
+++ servlet-demo/src/main/java/org/servlet/demo/filter/CurrentLimitingFilter.java
@@ -4,6 +4,7 @@ package org.servlet.demo.filter;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -15,6 +16,9 @@ public class CurrentLimitingFilter  implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+
+        ServletContext context = filterConfig.getServletContext();
+
 
     }
 
