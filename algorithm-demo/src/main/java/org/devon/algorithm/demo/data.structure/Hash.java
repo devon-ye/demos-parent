b2fb3c9150d9b40705b472abed7f90ab8faf3f3a
dewen.ye <dewen.ye@qunar.com>
2019-01-15 23:37:18 +0800

    [update]  ...

--- algorithm-demo/src/main/java/org/devon/algorithm/demo/data.structure/Hash.java
+++ algorithm-demo/src/main/java/org/devon/algorithm/demo/data.structure/Hash.java
@@ -3,8 +3,13 @@ package org.devon.algorithm.demo.data.structure;
 /**
  * @author devonmusa
  * @description:
- * @datetime ${date} ${time}
  * @since
  */
 public class Hash {
+
+    private Object[] data;
+
+    private class Node{
+
+    }
 }
