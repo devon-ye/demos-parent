ee44a22399f65371be69ef831dde90609c1f2328
dewen.ye <dewen.ye@qunar.com>
2019-01-19 18:21:48 +0800

    [update]  ...

--- zeroc-ice-demo/src/main/java/org/devon/zero/ice/demo/ZeroIceDemo.java
+++ zeroc-ice-demo/src/main/java/org/devon/zero/ice/demo/ZeroIceDemo.java
@@ -11,12 +11,7 @@ public class ZeroIceDemo {
         try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args))
         {
             com.zeroc.Ice.ObjectPrx base = communicator.stringToProxy("SimplePrinter:default -p 10000");
-            Demo.PrinterPrx printer = Demo.PrinterPrx.checkedCast(base);
-            if(printer == null)
-            {
-                throw new Error("Invalid proxy");
-            }
-            printer.printString("Hello World!");
+          
         }
     }
 }
