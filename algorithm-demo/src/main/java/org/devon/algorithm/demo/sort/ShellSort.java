0a1f038eb4e9cd86835d0b0e878330831df32284
dewen.ye <dewen.ye@qunar.com>
2019-01-23 00:19:49 +0800

    [update]  ...

--- algorithm-demo/src/main/java/org/devon/algorithm/demo/sort/ShellSort.java
+++ algorithm-demo/src/main/java/org/devon/algorithm/demo/sort/ShellSort.java
@@ -6,44 +6,41 @@ package org.devon.algorithm.demo.sort;
  * @datetime 2018/6/11 7:23
  * @since 1.0.0
  */
-public class ShellSort {
+public class ShellSort extends AbstractSort implements Sort {
 
 
     /**
      * 基于分治法思想，将数组跳跃式分为n个子数组插入排序，再次分为n/2个子数组...直到最后一个子数组
+     *
      * @param array
      * @return
      */
-    public static int[] sort(int[] array) {
+    public static Comparable[] sort(Comparable[] array) {
         if (array == null) {
             return null;
         }
         int j = 0;
-        int temp = 0;
+        Comparable temp = 0;
         for (int d = array.length / 2; d > 0; d /= 2) {
             for (int i = d; i < array.length; i++) {
                 temp = array[i];
                 for (j = i - d; j >= 0; j -= d) {
-                    if (temp < array[j]) {
+                    if (less(temp, array[j])) {
                         array[j + d] = array[j];
                     } else {
                         break;
                     }
                 }
                 array[j + d] = temp;
-
             }
-
-
         }
-
         return array;
 
     }
 
 
     public static void main(String[] args) {
-        int[] array = new int[]{6, 2, 1, 7, 9, 4, 3, 8, 5, 10};
+        Integer[] array = new Integer[]{6, 2, 1, 7, 9, 4, 3, 8, 5, 10};
         ShellSort.sort(array);
     }
 
