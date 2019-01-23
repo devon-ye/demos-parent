c3df628441cf889eb0d03134b86c6ec8acdadf30
dewen.ye <dewen.ye@qunar.com>
2019-01-23 08:40:40 +0800

    [update]  ice

--- zeroc-ice-demo/src/main/java/org/devon/zero/ice/demo/ZeroIceDemo.java
+++ zeroc-ice-demo/src/main/java/org/devon/zero/ice/demo/ZeroIceDemo.java
@@ -6,12 +6,18 @@ package org.devon.zero.ice.demo;
  */
 public class ZeroIceDemo {
 
-    public static void main(String[] args)
-    {
-        try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args))
-        {
+    public static void main(String[] args) {
+        int status = 0;
+        try (com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args)) {
             com.zeroc.Ice.ObjectPrx base = communicator.stringToProxy("SimplePrinter:default -p 10000");
-          
+            //
+            //注册关闭挂钩以在JVM关闭期间销毁通信器
+            //
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                communicator.destroy();
+            }));
         }
+        // communicator在这里自动销毁
+        System.exit(status);
     }
 }
