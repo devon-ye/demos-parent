b82e2015ff076ba2e2cf84ff1b6e707a74b3dbfe
Devonmusa <Devonmusa2015@gmail.com>
2019-01-26 00:41:24 +0800

    [update]  test

---
+++ algorithm-demo/src/test/java/org/devon/algorithm/demo/leetcode/StringDealTest.java
@@ -0,0 +1,37 @@
+package org.devon.algorithm.demo.leetcode;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author dewen.ye
+ * @description
+ * @date 2019/1/26 00:31
+ * @since ${project.version}
+ */
+public class StringDealTest {
+    private StringDeal stringDeal;
+
+    @Before
+    private void setUp() {
+        stringDeal = new StringDeal();
+    }
+
+    @Test
+    public void longestCommonPrefix1() {
+        String[] arsg = {"qewlqwoe", "qewljtuyet", "qewljeqweeqw", "qewlpppoo"};
+        try {
+            Assert.assertFalse("qewl".equals(stringDeal.longestCommonPrefix1(arsg)));
+        } catch (Exception e) {
+        }
+    }
+
+    @Test
+    public void longestCommonPrefixs() {
+        String[] arsg = {"abddwqeeeebbc", "abddwqeeeb", "abddwqee", "abddwqe"};
+        Assert.assertFalse("abddwqe".equals(stringDeal.longestCommonPrefix2(arsg)));
+    }
+}
