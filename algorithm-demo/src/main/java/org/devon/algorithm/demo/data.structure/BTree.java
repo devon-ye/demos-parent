fcc883854bc26208994d03b76ba8b1e3b53af181
Devonmusa <Devonmusa2015@gmail.com>
2019-02-04 01:43:31 +0800

    [update] balance tree

--- algorithm-demo/src/main/java/org/devon/algorithm/demo/data.structure/BTree.java
+++ algorithm-demo/src/main/java/org/devon/algorithm/demo/data.structure/BTree.java
@@ -1,12 +1,31 @@
 package org.devon.algorithm.demo.data.structure;
 
 /**
+ *  balance tree
  * @author devonmusa
- * @description:
  * @datetime 2018/6/5 17:01
  * @since 1.0.0
  */
-public class BTree {
+public class BTree<K extends Comparable<K>,V> {
 
+   private static  int childSize = 6;
+
+    private static class Node{
+        private long key;
+        private Entry[] children=new Entry[childSize];
+        private Node next;
+    }
+
+    private static class Entry{
+        private Comparable K;
+        private Object V;
+        private Node next;
+
+        public Entry(Comparable k, Object v, Node next) {
+            K = k;
+            V = v;
+            this.next = next;
+        }
+    }
 
 }
