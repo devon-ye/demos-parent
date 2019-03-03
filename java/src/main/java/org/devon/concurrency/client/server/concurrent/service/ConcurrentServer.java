96d56b1fe2c5dda53cce72768c84a3d1cdbc083c
Devonmusa <Devonmusa2015@gmail.com>
2019-03-03 21:31:04 +0800

    [add]   client server concurrent

---
+++ java/src/main/java/org/devon/concurrency/client/server/concurrent/service/ConcurrentServer.java
@@ -0,0 +1,34 @@
+package org.devon.concurrency.client.server.concurrent.service;
+
+import java.net.ServerSocket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author dewen.ye
+ * @date 2019/3/3 20:55
+ */
+public class ConcurrentServer {
+
+    private static ThreadPoolExecutor executor;
+    private static ServerSocket serverSocket;
+    private static ParallelCache cache;
+    private static volatile boolean stoped = false;
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        cache = new ParallelCache();
+        //
+
+    }
+
+    public static ParallelCache getCache() {
+        return cache;
+    }
+
+    public static ThreadPoolExecutor getExecutor() {
+        return executor;
+    }
+}
---
+++ java/src/main/java/org/devon/concurrency/client/server/concurrent/service/ConcurrentStatusCommand.java
@@ -0,0 +1,26 @@
+package org.devon.concurrency.client.server.concurrent.service;
+
+import org.devon.concurrency.client.server.serial.client.Command;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @author dewen.ye
+ * @date 2019/3/3 21:27
+ */
+public class ConcurrentStatusCommand extends Command {
+
+    public ConcurrentStatusCommand(String[] command) {
+        super(command);
+        setCacheable(false);
+    }
+
+    @Override
+    public String execute() {
+        StringBuilder sb = new StringBuilder();
+        ThreadPoolExecutor executor = ConcurrentServer.getExecutor();
+        return sb.toString();
+    }
+
+
+}
