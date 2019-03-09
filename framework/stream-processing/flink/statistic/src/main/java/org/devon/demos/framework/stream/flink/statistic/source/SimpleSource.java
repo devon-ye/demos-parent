b8b06646abfb8b1b52b76fb82e0cd6a192f2bdda
Devonmusa <Devonmusa2015@gmail.com>
2019-03-09 21:22:08 +0800

    [add]   flink self define source

---
+++ framework/stream-processing/flink/statistic/src/main/java/org/devon/demos/framework/stream/flink/statistic/source/SimpleSource.java
@@ -0,0 +1,39 @@
+package org.devon.demos.framework.stream.flink.statistic.source;
+
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+
+/**
+ * @author dewen.ye
+ * @date 2019/3/9 21:10
+ */
+public class SimpleSource implements SourceFunction<String>, CheckpointedFunction {
+  private long count = 0L;
+   private volatile boolean isRunning = true;
+    @Override
+    public void snapshotState(FunctionSnapshotContext functionSnapshotContext) throws Exception {
+
+    }
+
+    @Override
+    public void initializeState(FunctionInitializationContext functionInitializationContext) throws Exception {
+
+    }
+
+    @Override
+    public void run(SourceContext<String> sourceContext) throws Exception {
+            while (isRunning && count <1000){
+               synchronized (sourceContext.getCheckpointLock()){
+                   sourceContext.collect(count+"");
+                   count++;
+               }
+            }
+    }
+
+    @Override
+    public void cancel() {
+           isRunning = false;
+    }
+}
