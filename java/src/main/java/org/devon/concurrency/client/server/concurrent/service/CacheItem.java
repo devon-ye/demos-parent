564a978c9aa11db8c63e8b71bccc9c0857b33174
Devonmusa <Devonmusa2015@gmail.com>
2019-03-12 08:20:15 +0800

    [add] finished parallel algorithm

--- java/src/main/java/org/devon/concurrency/client/server/concurrent/service/CacheItem.java
+++ java/src/main/java/org/devon/concurrency/client/server/concurrent/service/CacheItem.java
@@ -9,7 +9,29 @@ import org.devon.concurrency.client.server.serial.client.Command;
 public class CacheItem {
 
     private Command command;
+    private String commands;
+    private String reponse;
     private long createTime;
-    private long lookTime;
+    private long accessTime;
 
+    public CacheItem(String commands, String reponse) {
+        this.commands = commands;
+        this.reponse = reponse;
+    }
+
+    public long getAccessTime() {
+        return accessTime;
+    }
+
+    public void setAccessTime(long accessTime) {
+        this.accessTime = accessTime;
+    }
+
+    public String getReponse() {
+        return reponse;
+    }
+
+    public void setReponse(String reponse) {
+        this.reponse = reponse;
+    }
 }
