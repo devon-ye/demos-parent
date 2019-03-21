fcd16e8c77f9021df8e54f5990710093364cae40
Devonmusa <Devonmusa2015@gmail.com>
2019-03-21 23:35:23 +0800

    [update] design

--- java/src/main/java/org/devon/java8/completable/future/LoadTask.java
+++ java/src/main/java/org/devon/java8/completable/future/LoadTask.java
@@ -1,12 +1,11 @@
 package org.devon.java8.completable.future;
 
 
-
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 import java.util.function.Supplier;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
@@ -15,9 +14,9 @@ import java.util.stream.Collectors;
  */
 public class LoadTask implements Supplier<List<Product>> {
 
-    private String path;
+    private Path path;
 
-    public LoadTask(String path) {
+    public LoadTask(Path path) {
         this.path = path;
     }
 
@@ -27,10 +26,10 @@ public class LoadTask implements Supplier<List<Product>> {
         try {
             //todo
             productList = Files.walk(path, FileVisitOption.FOLLOW_LINKS)
-                            .parallel().filter(f->f.toString()
+                    .parallel().filter(f -> f.toString()
                             .endsWith(".txt")).map(ProductLoader::load)
-                             .collect(Collectors.toList());
-        }catch (Exception e){
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
 
         }
         return null;
