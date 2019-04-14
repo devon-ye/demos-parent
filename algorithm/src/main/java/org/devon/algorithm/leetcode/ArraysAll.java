2e0c9a2c9068b2266beb6209ec3a1f3df9a2e8c3
dewen.ye <dewen.ye@qunar.com>
2019-04-15 07:38:28 +0800

    update algorithm

--- algorithm/src/main/java/org/devon/algorithm/leetcode/ArraysAll.java
+++ algorithm/src/main/java/org/devon/algorithm/leetcode/ArraysAll.java
@@ -16,11 +16,32 @@ public class ArraysAll {
         return i + 1;
     }
 
+    public static int removeElement(int[] nums, int val) {
+        if(nums.length == 0 ){
+            return 0;
+        }
+
+        int index = 0;
+
+        for(int i = 0; i < nums.length; i++){
+
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
+            }
+
+        }
+        return index;
+
+    }
+
 
     public static void main(String[] args) {
         int[] a = {0, 0, 1, 2, 3, 3, 3, 4};
         ArraysAll.removeDuplicatesSortedArray(a);
         int[]  b= {0, 1, 1, 2, 3, 3, 3, 4};
         ArraysAll.removeDuplicatesSortedArray(b);
+
+        ArraysAll.removeElement(b,3);
     }
 }
