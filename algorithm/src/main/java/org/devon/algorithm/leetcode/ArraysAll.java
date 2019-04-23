758441f298d0b53a312bcfa925aa4cb0eb6928d6
dewen.ye <dewen.ye@qunar.com>
2019-04-23 08:24:01 +0800

    update test case

--- algorithm/src/main/java/org/devon/algorithm/leetcode/ArraysAll.java
+++ algorithm/src/main/java/org/devon/algorithm/leetcode/ArraysAll.java
@@ -20,16 +20,12 @@ public class ArraysAll {
         if (nums.length == 0) {
             return 0;
         }
-
         int index = 0;
-
         for (int i = 0; i < nums.length; i++) {
-
             if (nums[i] != val) {
                 nums[index] = nums[i];
                 index++;
             }
-
         }
         return index;
 
