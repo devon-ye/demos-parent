1007089b3be12850d1c9fd8788004a7eb33ffe12
dewen.ye <dewen.ye@qunar.com>
2019-04-27 00:45:49 +0800

    add module-info

--- demos-parent/algorithm/src/main/java/module-info.java
+++ demos-parent/algorithm/src/main/java/module-info.java
@@ -1,3 +1,10 @@
+import org.devon.algorithm.service.AlgorithmService;
+
 module org.devon.algorithm {
     exports org.devon.algorithm.service;
+   // provides org.devon.algorithm.service.AlgorithmService with  impl class;
+   // opens
+   // uses
+   // requires
+   // requires transitive
 }
