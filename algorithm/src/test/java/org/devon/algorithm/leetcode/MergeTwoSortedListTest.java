2be26d0a7b92339d2888851bb8690f85efdddf1f
dewen.ye <dewen.ye@qunar.com>
2019-04-28 08:46:54 +0800

    update listNode swapPairs

--- algorithm/src/test/java/org/devon/algorithm/leetcode/MergeTwoSortedListTest.java
+++ algorithm/src/test/java/org/devon/algorithm/leetcode/MergeTwoSortedListTest.java
@@ -144,4 +144,26 @@ public class MergeTwoSortedListTest {
 
         MergeTwoSortedList.mergeKLists(lists);
     }
+
+    @Test
+    public void swapPairs() {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(7);
+        ListNode node4 = new ListNode(9);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        MergeTwoSortedList.swapPairs(node1);
+    }
+
+    @Test
+    public void swapPairs2() {
+        ListNode node5 = new ListNode(1);
+        ListNode node6 = new ListNode(2);
+        ListNode node7 = new ListNode(7);
+        node5.next =node6;
+        node6.next = node7;
+        MergeTwoSortedList.swapPairs(node5);
+    }
 }
