bc117d47fa6fe19fcaa5419a3e3e65172d96538d
dewen.ye <dewen.ye@qunar.com>
2019-05-18 15:35:10 +0800

    add  linked list  reverse

--- algorithm/src/main/java/org/devon/algorithm/leetcode/LinkedListOpAlgorithm.java
+++ algorithm/src/main/java/org/devon/algorithm/leetcode/LinkedListOpAlgorithm.java
@@ -1,14 +1,92 @@
 package org.devon.algorithm.leetcode;
 
-public class LinkedListOpAlgorithm {
+import java.util.*;
 
-    public static ListNode findMiddleNode(ListNode head){
-        ListNode  fast = head;
+public class LinkedListOpAlgorithm {
+    /**
+     * complexity analysis
+     * T(n) = n + n + n + n = 4n
+     * <p>Time complexity</p> O(n)
+     * <p>Space complexity</p> O(0)
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode findMiddleNode(ListNode head) {
+        ListNode fast = head;
         ListNode slow = head;
-        while (fast != null && fast.next != null){
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
         return slow;
+    }
+
+    /**
+     * complexity analysis
+     * T(n) = n + n + n + 2n +n =6n
+     * Time complexity: O(n)
+     * Space complexity: O(1)
+     * @param head
+     * @return
+     */
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     *   if has cycle,  fast pointer already meeting slow pointer
+     * complexity analysis
+     * T(n) = n + n + n =3n
+     * Time complexity: O(n)
+     * S(n) = n
+     * Space complexity: O(n)
+     * @param head
+     * @return
+     */
+    public static boolean hasCycle1(ListNode head) {
+        Set<String> set = new HashSet<>();
+        while (head != null) {
+            if (set.contains(head)) {
+                return true;
+            } else {
+                set.add(head.toString());
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+    /**
+     * complexity analysis
+     * T(n) = n + n + n + n + n + n = 6n
+     * Time complexity:O(n)
+     * S(n) = 1
+     * Space complexity:O(1)
+     * @param head
+     * @return
+     */
+    public static ListNode reverseList(ListNode head){
+        ListNode result = new ListNode(0);
+        ListNode dummy = null;
+        while(head != null){
+             //取出单节点
+              dummy = head;
+              head = head.next;
+              dummy.next = null;
+
+              dummy.next = result.next;
+              result.next = dummy;
+        }
+        return result.next;
     }
 }
--- algorithm/src/test/java/org/devon/algorithm/leetcode/LinkedListOpAlgorithmTest.java
+++ algorithm/src/test/java/org/devon/algorithm/leetcode/LinkedListOpAlgorithmTest.java
@@ -19,4 +19,42 @@ public class LinkedListOpAlgorithmTest {
         node3.next = node4;
         Assert.assertEquals(3,LinkedListOpAlgorithm.findMiddleNode(node1).val);
     }
+
+    @Test
+    public void hasCycle1() {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node2;
+        Assert.assertTrue(LinkedListOpAlgorithm.hasCycle1(node1));
+    }
+
+    @Test
+    public void reverseList() {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        Assert.assertEquals(4,LinkedListOpAlgorithm.reverseList(node1).val);
+    }
+
+    @Test
+    public void hasCycle() {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node2;
+        Assert.assertTrue(LinkedListOpAlgorithm.hasCycle1(node1));
+    }
 }
