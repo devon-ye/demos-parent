a0683888d299fd9f15eb0826913acb9667c873f3
dewen.ye <dewen.ye@qunar.com>
2019-05-18 16:15:15 +0800

    add  linked list  remove elements

--- algorithm/src/main/java/org/devon/algorithm/leetcode/LinkedListOpAlgorithm.java
+++ algorithm/src/main/java/org/devon/algorithm/leetcode/LinkedListOpAlgorithm.java
@@ -89,4 +89,50 @@ public class LinkedListOpAlgorithm {
         }
         return result.next;
     }
+
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        //快指针先走n+1
+        for (int i = 1; i <= n + 1; i++) {
+            fast = fast.next;
+        }
+        //快慢指针一块走 快指针到尾节点，满指针才到倒数n-1
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+
+    public static ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        int length = 0;
+        ListNode first = head;
+        while (first != null) {
+            length++;
+            first = first.next;
+        }
+        //removeIndex = (L-N)+1 添加辅助节点后变为removeIndex =（L-N）
+        length -= n;
+        first = dummy;
+        //用辅助节点从链表头部走到 removeIndex
+        while (length > 0) {
+            length--;
+            first = first.next;
+        }
+        first.next = first.next.next;
+        return dummy.next;
+    }
+
+    public static ListNode removeElements(ListNode head, int val) {
+        if (head == null) return null;
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+    }
 }
