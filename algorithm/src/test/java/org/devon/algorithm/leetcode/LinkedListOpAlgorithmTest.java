35d38456a68fea10abfcfed921b661fd8ddaba3d
dewen.ye <dewen.ye@qunar.com>
2019-05-19 21:52:43 +0800

    add  linked list  reverse

--- algorithm/src/test/java/org/devon/algorithm/leetcode/LinkedListOpAlgorithmTest.java
+++ algorithm/src/test/java/org/devon/algorithm/leetcode/LinkedListOpAlgorithmTest.java
@@ -57,4 +57,50 @@ public class LinkedListOpAlgorithmTest {
         node4.next = node2;
         Assert.assertTrue(LinkedListOpAlgorithm.hasCycle1(node1));
     }
+
+    @Test
+    public void removeNthFromEnd() {
+        //case 1
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        LinkedListOpAlgorithm.removeNthFromEnd(node1, 2);
+
+    }
+
+    @Test
+    public void removeNthFromEnd2() {
+        //case 2
+        ListNode node = new ListNode(1);
+        LinkedListOpAlgorithm.removeNthFromEnd2(node, 1);
+    }
+
+    @Test
+    public void removeElements() {
+
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        Assert.assertEquals(1,LinkedListOpAlgorithm.removeElements(node1,3).val);
+    }
+
+    @Test
+    public void isPalindrome() {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        node1.next = node2;
+
+        Assert.assertTrue(!LinkedListOpAlgorithm.isPalindrome(node1));
+    }
 }
