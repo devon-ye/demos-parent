9a748db41181227e35f873df792bb9d90ab19eff
dewen.ye <dewen.ye@qunar.com>
2019-06-17 23:52:28 +0800

    update algorithm

--- algorithm/src/main/java/org/devon/algorithm/leetcode/LinkedListOpAlgorithm.java
+++ algorithm/src/main/java/org/devon/algorithm/leetcode/LinkedListOpAlgorithm.java
@@ -259,4 +259,22 @@ public class LinkedListOpAlgorithm {
         dummy.next= second.next;
         return first.next;
     }
+
+    public int numComponents(ListNode head, int[] G) {
+        Set<Integer> Gset = new HashSet();
+        for (int x: G) Gset.add(x);
+
+        ListNode cur = head;
+        int ans = 0;
+
+        while (cur != null) {
+            if (Gset.contains(cur.val) &&
+                    (cur.next == null || !Gset.contains(cur.next.val)))
+                ans++;
+            cur = cur.next;
+        }
+
+        return ans;
+    }
+
 }
