c31b31fdb502cc6d8f7dfe1a787958c5f95d3f1b
dewen.ye <dewen.ye@qunar.com>
2019-08-17 15:43:12 +0800

    update abstract  method

--- algorithm/src/main/java/org/devon/algorithm/sort/AbstractSort.java
+++ algorithm/src/main/java/org/devon/algorithm/sort/AbstractSort.java
@@ -1,5 +1,6 @@
 package org.devon.algorithm.sort;
 
+
 /**
  * @author dewen.ye
  * @date 2019/1/21 23:57
@@ -12,11 +13,48 @@ public abstract class AbstractSort {
         array[j] = t;
     }
 
+    public static <T> void exch(T[] array, int i, int j) {
+        T t = array[i];
+        array[i] = array[j];
+        array[j] = t;
+    }
+
     public static boolean less(Comparable v, Comparable w) {
         return v.compareTo(w) < 0;
     }
 
+    public static <T> boolean less(T v, T w) {
+        Comparable vc = (Comparable) v;
+       /* Comparable vw = (Comparable) w;*/
+        return vc.compareTo(w) < 0;
+    }
 
+    public static <T> Object[] newTypeTArray(Class<T> clazzType, int length) {
+        String typeName = clazzType.getSimpleName();
+        switch (typeName) {
+            case "Double[]":
+                return new Double[length];
+            case "Integer[]":
+                return new Integer[length];
+            case "Byte[]":
+                return new Byte[length];
+            case "Short[]":
+                return new Short[length];
+            case "Long[]":
+                return new Long[length];
+            case "Boolean[]":
+                return new Boolean[length];
+            case "Character[]":
+                return new Character[length];
+            case "String[]":
+                return new String[length];
+            case "Object[]":
+                return new Object[length];
+            default:
+                throw new IllegalArgumentException("unsupported type");
+        }
+
+    }
 
 
 }
