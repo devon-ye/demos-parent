24d2c144adce714abd755d4cf779fa48c1af499e
devon <Devonmusa2015@gmail.com>
2019-10-02 10:34:09 +0800

    update

---
+++ framework/websocket/src/main/java/org/devon/framework/websocket/WebSocketHandler.java
@@ -0,0 +1,11 @@
+package org.devon.framework.websocket;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class WebSocketHandler extends SimpleChannelInboundHandler<Object>{
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Object o) throws Exception {
+
+    }
+}
