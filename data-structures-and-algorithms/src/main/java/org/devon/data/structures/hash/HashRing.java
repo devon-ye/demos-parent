96502eee6d601402e3a3f2afe9dcfeda8fca5185
devon <Devonmusa2015@gmail.com>
2019-11-06 23:57:27 +0800

    update

--- data-structures-and-algorithms/src/main/java/org/devon/data/structures/hash/HashRing.java
+++ data-structures-and-algorithms/src/main/java/org/devon/data/structures/hash/HashRing.java
@@ -13,4 +13,41 @@ package org.devon.data.structures.hash;
  * @date 2019/2/16 16:15
  */
 public class HashRing {
+
+	private static final int DEFAULT_CAPACITY=10000;
+
+	private volatile int readIndex;
+	private volatile int writeIndex;
+
+     private Object[] data;
+
+     private int size;
+     private int capacity;
+
+
+
+	public HashRing() {
+		this(DEFAULT_CAPACITY);
+	}
+
+	public HashRing(int capacity){
+		data =new Object[capacity];
+		this.capacity = capacity;
+	}
+
+	public void write(Object obj){
+		if(size > capacity) throw new RuntimeException("HashRing is full!");
+
+		data[writeIndex] = obj;
+		writeIndex++;
+		if(writeIndex >= capacity) writeIndex = 0;
+	}
+
+	public Object read() {
+		if(size ==0) throw new RuntimeException("HashRing is empty!");
+		Object obj = data[readIndex];
+		readIndex++;
+		if(readIndex >= capacity) readIndex = 0;
+		return obj;
+	}
 }
