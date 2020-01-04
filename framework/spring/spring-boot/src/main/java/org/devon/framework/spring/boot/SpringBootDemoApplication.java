8f943f2ad8d0722d1bd29633d803eeb01727a29c
dewen.ye <dewen.ye@qunar.com>
2020-01-04 21:03:48 +0800

    update http2 server

--- framework/spring/spring-boot/src/main/java/org/devon/framework/spring/boot/SpringBootDemoApplication.java
+++ framework/spring/spring-boot/src/main/java/org/devon/framework/spring/boot/SpringBootDemoApplication.java
@@ -1,12 +1,27 @@
 package org.devon.framework.spring.boot;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http2.Http2Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+	/**
+	 * 明文http2协议服务
+	 */
+	public WebServerFactoryCustomizer<TomcatServletWebServerFactory> tomcatCustomizer(){
+		return factory -> {
+			Connector connector = new Connector();
+			connector.addUpgradeProtocol(new Http2Protocol());
+			connector.setPort(8080);
+			factory.addAdditionalTomcatConnectors(connector);
+		};
 	}
 }
