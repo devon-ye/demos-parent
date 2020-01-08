a98a5e0d1bfd1806d15a6c2d1dccb3829f584237
dewen.ye <dewen.ye@qunar.com>
2020-01-09 00:20:20 +0800

    update

--- data-structures-and-algorithms/src/main/java/org/devon/algorithms/leetcode/ParenthesesValid.java
+++ data-structures-and-algorithms/src/main/java/org/devon/algorithms/leetcode/ParenthesesValid.java
@@ -1,6 +1,8 @@
 package org.devon.algorithms.leetcode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 
@@ -35,5 +37,24 @@ public class ParenthesesValid {
 
         return stack.isEmpty();
 
+    }
+
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+    }
+
+    public static void backtrack(List<String> ans, String cur, int open, int close, int max){
+        if (cur.length() == max * 2) {
+            ans.add(cur);
+            return;
+        }
+
+        if (open < max)
+            backtrack(ans, cur+"(", open+1, close, max);
+        if (close < open)
+            backtrack(ans, cur+")", open, close+1, max);
     }
 }
