9e8fa557b5af190a027774fe1e9f4a86bd45ec90
dewen.ye <dewen.ye@qunar.com>
2020-02-08 22:09:46 +0800

    update move

---
+++ language/java/src/main/java/org/devon/jvm/referrence/Referrence.java
@@ -0,0 +1,12 @@
+package org.devon.jvm.referrence;
+
+import java.lang.ref.SoftReference;
+
+/**
+ * @author devon.ye
+ * @datetime 2020/2/7 10:06 下午
+ * @since
+ */
+public class Referrence {
+	private SoftReference
+}
