43b1fcd4ea3957b5ff0c152c55b82026acea9a8c
dewen.ye <dewen.ye@qunar.com>
2020-02-11 23:36:15 +0800

    update move

--- language/java/src/main/java/org/devon/data/string/StringUtils.java
+++ language/java/src/main/java/org/devon/data/string/StringUtils.java
@@ -2,7 +2,9 @@ package org.devon.data.string;
 
 
 import org.apache.commons.lang3.time.DateFormatUtils;
+import sun.misc.Unsafe;
 
+import java.lang.reflect.Field;
 import java.util.Date;
 import java.util.Random;
 
@@ -56,6 +58,76 @@ public class StringUtils {
 			count = result.length();
 		}
 		return result;
+	}
+
+	public static void main(String[] args) {
+		String s1 = "uml";
+		String s2 = "uml";
+		String s3 = new String("uml");
+		String s4 = new String("uml");
+		printAddresses("s1:", s1);
+		printAddresses("s2:", s2);
+		printAddresses("s3:", s3);
+		printAddresses("s4:", s4);
+
+		if (s1 == s2) {
+			System.out.println(true);
+		}
+		if (s3 == s4) {
+			System.out.println(true);
+		}
+		if (s1.equals(s3)) {
+			System.out.println(true);
+		}
+		float[] f[][] = new float[6][6][7];
+		System.out.println(f);
+		try {
+			throw new RuntimeException();
+			//System.out.println("error1");
+		} catch (Exception e) {
+			System.out.println("error2");
+		}
+		System.out.println("error3");
+	}
+
+
+	private static Unsafe getUnsafe() {
+		try {
+			Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+			theUnsafe.setAccessible(true);
+			return (Unsafe) theUnsafe.get(null);
+		} catch (Exception e) {
+			throw new AssertionError(e);
+		}
+	}
+
+	static final Unsafe unsafe = getUnsafe();
+	static final boolean is64bit = true;
+
+	public static void printAddresses(String label, Object... objects) {
+		System.out.print(label + ": 0x");
+		long last = 0;
+		int offset = unsafe.arrayBaseOffset(objects.getClass());
+		int scale = unsafe.arrayIndexScale(objects.getClass());
+		switch (scale) {
+			case 4:
+				long factor = is64bit ? 8 : 1;
+				final long i1 = (unsafe.getInt(objects, offset) & 0xFFFFFFFFL) * factor;
+				System.out.print(Long.toHexString(i1));
+				last = i1;
+				for (int i = 1; i < objects.length; i++) {
+					final long i2 = (unsafe.getInt(objects, offset + i * 4) & 0xFFFFFFFFL) * factor;
+					if (i2 > last)
+						System.out.print(", +" + Long.toHexString(i2 - last));
+					else
+						System.out.print(", -" + Long.toHexString(last - i2));
+					last = i2;
+				}
+				break;
+			case 8:
+				throw new AssertionError("Not supported");
+		}
+		System.out.println();
 	}
 
 }
