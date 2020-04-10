bbe1ae4849cd8610f245b73fa316926248025c6d
dewen.ye <dewen.ye@qunar.com>
2020-04-11 02:00:58 +0800

    go web init

--- language/go/src/go.go
+++ language/go/src/go.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"strings"
 )
 
 func main() {
-	http.HandleFunc("/", sayHelloName)       //设置访问的路由
+	http.HandleFunc("/", sayHelloName) //设置访问的路由
+
+	http.HandleFunc("/login", login) //设置访问的路由
+
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
 
 	if err != nil {
@@ -35,4 +39,20 @@ func sayHelloName(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, "HELLO astaxie!")
 
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("method：", r.Method)
+
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.gtpl")
+
+		t.Execute(w, nil)
+	} else {
+		fmt.Println("username", r.Form["username"])
+
+		fmt.Println("password", r.Form["password"])
+
+	}
 }
