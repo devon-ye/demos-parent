ab3e345b342671af3832267711019886aa45d93a
dewen.ye <dewen.ye@qunar.com>
2020-04-24 09:27:20 +0800

    UPDATE RUST

--- language/java/src/main/java/org/devon/concurrency/pool/ThreadPoolUtil.java
+++ language/java/src/main/java/org/devon/concurrency/pool/ThreadPoolUtil.java
@@ -1,7 +1,15 @@
 package org.devon.concurrency.pool;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.ThreadMXBean;
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author devonmusa
@@ -17,9 +25,12 @@ public class ThreadPoolUtil {
     private static final Executor  singleScheduleThreadPool = Executors.newSingleThreadScheduledExecutor();
     private static final  Executor scheduleThreadPool = Executors.newScheduledThreadPool(2);
     private static final Executor WorkStealingPool = Executors.newWorkStealingPool();
-
+     private ThreadPoolExecutor executorService = new ThreadPoolExecutor(1,1,1, TimeUnit.MILLISECONDS,new LinkedBlockingQueue<>(2))
 
     private ThreadPoolUtil() {
+       ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+        MemoryMXBean memoryMXBean =ManagementFactory.getMemoryMXBean();
+        ManagementFactory.getPlatformMBeanServer().getDefaultDomain();
     }
 
 
--- language/rust/README.md
+++ language/rust/README.md
@@ -25,6 +25,67 @@ fn fn_name(paramter1:type1,parameter2:type2) -> returnType{
 
 ## 数据类型
 
+- 有符号
+    
+    i8、i16、i32、i64、i128 isize
+- 无符号
+
+    u8、u16u32、u64、u128、usize
+- 浮点
+    f32、f64
+- 字符
+    char
+- 布尔
+    bool 
+- 元组
+   Tuple()
+       
+       例如：    let tuple = (1, "hello", 4.5, true);
+- 数组
+   Arrays
+   
+        例如：    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+
+- 切片
+   Slices
+    
+        例如：
+- 结构体
+    struct
+    
+        例如：   
+```rust
+     // A unit struct
+        struct Nil;
+
+    // A tuple struct
+    struct Pair(i32, f32);
+
+    // A struct with two fields
+    struct Point {
+        x: f32,
+        y: f32,
+    }
+```
+- 枚举
+``` RUST
+    enum typeN{
+        // An `enum` may either be `unit-like`,
+        PageLoad,
+        PageUnload,
+        // like tuple structs,
+        KeyPress(char),
+        Paste(String),
+        // or c-like structures.
+        Click { x: i64, y: i64 },
+    }
+```
+- 常量
+
+    static LANGUAGE: &str = "Rust";
+    
+    const THRESHOLD: i32 = 10;
+
 ## 流程控制
 
 ## 特征
