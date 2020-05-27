4cabb516d9ae1924b75a6cfb76467d0c315541e0
dewen.ye <dewen.ye@qunar.com>
2020-05-27 09:47:34 +0800

    rust basic

---
+++ language/rust/src/basic.rs
@@ -0,0 +1,41 @@
+fn main() {
+    let numbers = 45..100;
+    // let numbers : Range<i32> = 45 .. 100;
+
+    for i in numbers {
+        println!("This is number:{}", i);
+    }
+
+    let annimals = vec!["cat", "dog", "pig"];
+    // let annimals : Vec<&Str>= vec! {"cat", "dog", "pig"};
+
+    for a in annimals.iter() {
+        println!("this is annimal:{}", a);
+    }
+
+    for (index, a) in annimals.iter().enumerate() {
+        println!("this is index:{} annimal:{}", index, a);
+    }
+    direction();
+
+    let tuple1 = (1, "mamei", 3.78, true);
+    println!("this is tuple1.2={}", tuple1.2);
+}
+
+fn direction() {
+    let play_direction: Direction = Direction::UP;
+
+    match play_direction {
+        Direction::UP => println!("we are heading up!"),
+        Direction::DOWN => println!("we are heading up!"),
+        Direction::LEFT => println!("we are heading up!"),
+        Direction::RIGHT => println!("we are heading up!"),
+    }
+}
+
+enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+}
