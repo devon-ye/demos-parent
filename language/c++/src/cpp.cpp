9a474a8b31fea20461f20525e77c95b760092917
devon <devon.ye@foxmail.com>
2020-11-29 16:47:25 +0800

    [update] number and bool

--- language/c++/src/cpp.cpp
+++ language/c++/src/cpp.cpp
@@ -7,9 +7,20 @@
 //============================================================================
 
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "please input a number!" <<endl;
+	int x = 0;
+	cin>> x;
+	cout << oct << x <<endl;
+	cout << dec << x <<endl;
+	cout << hex << x <<endl;
+	cout << "please input a bool value (0 or 1)!" << endl;
+	bool y = false;
+	cin >> y;
+	cout << boolalpha << y << endl;
+	system("pause");
 	return 0;
 }
