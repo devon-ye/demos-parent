4ed577c02e6c8ba12b16158b8738aed1a2309c68
devon <devon.ye@foxmail.com>
2021-03-21 15:21:37 +0800

    [update] 1.数独库验证  2. 长整数相加  3.回文串判断

--- data-structures-and-algorithms/src/main/java/org/devon/algorithms/leetcode/LinkedListOpAlgorithm.java
+++ data-structures-and-algorithms/src/main/java/org/devon/algorithms/leetcode/LinkedListOpAlgorithm.java
@@ -403,6 +403,19 @@ public class LinkedListOpAlgorithm {
 		return result;
 	}
 
+	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		ListNode tempA = headA;
+		ListNode tempB = headB;
+		while(tempA != tempB){
+
+			//
+			tempA=tempA == null? headB:tempA.next;
+
+			tempB=tempB == null? headA:tempB.next;
+		}
+		return tempA;
+	}
+
 
 
 
